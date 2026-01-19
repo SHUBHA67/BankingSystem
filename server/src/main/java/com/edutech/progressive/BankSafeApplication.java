663bf6f9497e556ba8b573c7e27a8b0dba0d3b54
SHUBHA67 <shubhamc1203@gmail.com>
2026-01-19 13:03:22 +0000

    Day 4 Task

--- server/src/main/java/com/edutech/progressive/BankSafeApplication.java
+++ server/src/main/java/com/edutech/progressive/BankSafeApplication.java
@@ -1,5 +1,8 @@
 package com.edutech.progressive;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class BankSafeApplication {
     public static void main(String[] args) {
         System.out.println("Welcome to BankSafe Project!");
