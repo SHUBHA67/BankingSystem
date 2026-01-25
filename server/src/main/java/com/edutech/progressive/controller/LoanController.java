998ee5486c207a00e0495db25d5a8d5a07ff7580
SHUBHA67 <shubhamc1203@gmail.com>
2026-01-25 11:31:44 +0000

    Day 10 Task

--- server/src/main/java/com/edutech/progressive/controller/LoanController.java
+++ server/src/main/java/com/edutech/progressive/controller/LoanController.java
@@ -1,31 +1,63 @@
 package com.edutech.progressive.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.edutech.progressive.entity.Loan;
+import com.edutech.progressive.service.LoanService;
 
 import java.util.List;
 
+@RestController
+@RequestMapping("/loans")
 public class LoanController {
 
+    private final LoanService loanService;
+
+    @Autowired
+    public LoanController(LoanService loanService) {
+        this.loanService = loanService;
+    }
+
+    @GetMapping
     public ResponseEntity<List<Loan>> getAllLoans() {
-        return null;
+        return ResponseEntity.ok(loanService.getAllLoans());
     }
 
-    public ResponseEntity<Loan> getLoanById(Long id) {
-        return null;
+    @GetMapping("/{id}")
+    public ResponseEntity<Loan> getLoanById(@PathVariable Long id) {
+        Loan l = loanService.getLoanById(id);
+        if (l==null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(l);
     }
 
-    public ResponseEntity<Loan> createLoan(Loan loan) {
-        return null;
+    @PostMapping
+    public ResponseEntity<Loan> createLoan(@RequestBody Loan loan) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(loanService.createLoan(loan));
     }
 
-    public ResponseEntity<Void> updateLoan(Long id, Loan loan) {
-        return null;
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateLoan(@PathVariable Long id, @RequestBody Loan loan) {
+        Loan l = loanService.getLoanById(id);
+        // if (l==null) {
+        //     return ResponseEntity.notFound().build();
+        // }
+        loan.setId(id);
+        loanService.updateLoan(loan);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    public ResponseEntity<Void> deleteLoan(Long id) {
-        return null;
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteLoan(@PathVariable Long id) {
+        Loan l = loanService.getLoanById(id);
+        // if (l==null) {
+        //     return ResponseEntity.notFound().build();
+        // }
+        loanService.deleteLoan(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- server/src/main/java/com/edutech/progressive/repository/LoanRepository.java
+++ server/src/main/java/com/edutech/progressive/repository/LoanRepository.java
@@ -1,6 +1,8 @@
 package com.edutech.progressive.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.edutech.progressive.entity.Loan;
 
-public interface LoanRepository {
+public interface LoanRepository extends JpaRepository<Loan, Long>{
 }
